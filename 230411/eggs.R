e7a2fda237f2513375ede26d2d718c3abc6cae16
Jon Huck <46273070+huckjo@users.noreply.github.com>
2023-04-11 18:06:34 -0600

    Add graph of egg production

--- 230411/eggs.R
+++ 230411/eggs.R
@@ -4,42 +4,13 @@
 # Load data --------------------------------------------------------------------
 
 library(pacman)
-p_load(tidytuesdayR, tidyverse)
+p_load(cowplot, tidytuesdayR, tidyverse)
 
 # https://osf.io/z2gxn/
 
 tt <- tidytuesdayR::tt_load('2023-04-11')
 eggs <- tt$`egg-production`
 cagefree <- tt$`cage-free-percentages`
-
-# Explore data -----------------------------------------------------------------
-
-# plot the eggs data
-ggplot(eggs, aes(x = observed_month, y = n_eggs, 
-                 color = prod_type, linetype = prod_process)) +
-  geom_line(size = 1) +
-  labs(x = "Month", y = "Egg Production") +
-  facet_wrap(~ prod_process) +
-  theme_classic()
-
-# calculate the ratio of eggs to hens
-eggs$ratio <- eggs$n_eggs / eggs$n_hens
-
-# plot the eggs ratios
-ggplot(eggs, aes(x = observed_month, y = ratio, 
-                 color = prod_type, linetype = prod_process)) +
-  geom_line(size = 1) +
-  labs(x = "Month", y = "Egg Production") +
-  facet_wrap(~ prod_process) +
-  theme_classic()
-
-# plot cage free data
-cagefree |> 
-  filter(!is.na(percent_eggs)) |> 
-  ggplot() +
-  geom_line(aes(x = observed_month, y = percent_eggs), color="red") +
-  geom_line(aes(x = observed_month, y = percent_hens), color="blue") +
-  theme_classic()
 
 # Wrangle data -----------------------------------------------------------------
 
@@ -59,65 +30,110 @@ wide_eggs <- eggs |>
 
 # total eggs
 eggs_all <- wide_eggs |> 
-  mutate(prod_type = "table eggs") |> 
-  select(observed_month, prod_type, n_hens_all, n_eggs_all)
+  select(observed_month, n_hens_all, n_eggs_all)
 
 # cage-free eggs
-eggs_cagefree <- wide_eggs |> 
-  mutate(
-    n_eggs = n_eggs_organic + n_eggs_nonorganic,
-    n_hens = n_hens_organic + n_hens_nonorganic,
-    prod_type = "table eggs",
-    prod_process = "cage-free"
-  ) |> 
-  select(observed_month, prod_type, prod_process, n_hens, n_eggs)
+#eggs_cagefree <- wide_eggs |> 
+#  mutate(
+#    n_eggs = n_eggs_organic + n_eggs_nonorganic,
+#    n_hens = n_hens_organic + n_hens_nonorganic,
+#    prod_type = "table eggs",
+#    prod_process = "cage-free"
+#  ) |> 
+#  select(observed_month, prod_type, prod_process, n_hens, n_eggs)
   
-# caged eggs
+# subtract conventional eggs from total
 eggs_caged <- wide_eggs |> 
   mutate(
     n_eggs = n_eggs_all - (n_eggs_organic + n_eggs_nonorganic),
     n_hens = n_hens_all - (n_hens_organic + n_hens_nonorganic),
-    prod_type = "table eggs",
     prod_process = "caged"
   ) |> 
-  select(observed_month, prod_type, prod_process, n_hens, n_eggs)
+  select(observed_month, prod_process, n_hens, n_eggs)
 
 # percent caged and cage-free
-table_eggs <- eggs_caged |> 
-  bind_rows(eggs_cagefree) |> 
-  left_join(eggs_all, by = c("observed_month", "prod_type")) |> 
+table_eggs <- eggs |> 
+  filter(prod_type == "table eggs" & prod_process != "all") |> 
+  select(observed_month, prod_process, n_hens, n_eggs) |> 
+  bind_rows(eggs_caged) |> 
+  left_join(eggs_all, by = "observed_month") |> 
   mutate(percent_hens = n_hens / n_hens_all * 100,
          percent_eggs = n_eggs / n_eggs_all * 100) |> 
   filter(!is.na(percent_eggs))
 
-# Draw graph -------------------------------------------------------------------
+# Draw graphs ------------------------------------------------------------------
 
-table_eggs |> 
-  ggplot(aes(x = observed_month, y = percent_hens, color = prod_process)) +
-  geom_line(size = 1) +
-  labs(x = "Month", y = "Percent of Egg Production",
-       title = "Growing Percent of Eggs Are Cage-Free") +
-  theme_classic()
+vol <- table_eggs |> 
+  ggplot(aes(x = observed_month, y = n_eggs/1000000000, fill = prod_process)) +
+  geom_area() +
+  scale_fill_manual(values = c("#FB0000", "#EFAAA9", "#ECC301")) +
+  labs(x = "", y = "Eggs (billions)",
+       caption = " ") +
+  theme_classic() +
+  theme(
+    plot.caption = element_text(size = 8),
+    plot.background = element_rect(fill = "#fffaf0", color = "#fffaf0"),
+    panel.background = element_rect(fill = "#fffaf0"),
+    axis.text.y = element_text(size = 8),
+    axis.text.x = element_text(size = 8),
+    legend.position = "none",
+    plot.margin = margin(0, 0, .5, .5, "cm")
+  )
 
-# percent organic
-eggs |> 
-  filter(prod_process != "all") |> 
-  mutate(prod_process = case_when(
-    prod_process == "cage-free (non-organic)" ~ "non-organic",
-    prod_process == "cage-free (organic)" ~ "organic"
-  )) |> 
-  left_join(
-    eggs_cagefree |> 
-      select(-prod_process) |> 
-      rename(n_hens_all = n_hens, n_eggs_all = n_eggs),
-    by = c("observed_month", "prod_type")
-  ) |> 
-  mutate(percent_hens = n_hens / n_hens_all * 100,
-         percent_eggs = n_eggs / n_eggs_all * 100) |> 
-  ggplot(aes(x = observed_month, y = percent_hens, color = prod_process)) +
-  geom_line(size = 1) +
-  labs(x = "Month", y = "Percent of Egg Production",
-       title = "Growing Percent of Eggs Are Cage-Free") +
-  theme_classic()
+mix <- table_eggs |> 
+  ggplot(aes(x = observed_month, y = percent_eggs, fill = prod_process)) +
+  geom_area() +
+  scale_fill_manual(values = c("#FB0000", "#EFAAA9", "#ECC301")) +
+  labs(x = "", y = "% Eggs",
+       caption = "Source: Humane League | github.com/huckjo") +
+  theme_classic() +
+  theme(
+    plot.caption = element_text(size = 8),
+    plot.background = element_rect(fill = "#fffaf0", color = "#fffaf0"),
+    panel.background = element_rect(fill = "#fffaf0"),
+    axis.text.y = element_text(size = 8),
+    axis.text.x = element_text(size = 8),
+    legend.position = "none",
+    plot.margin = margin(0, .5, .5, 0, "cm")
+  )
 
+annual_eggs <- table_eggs |> 
+  mutate(year = format(observed_month, "%Y")) |> 
+  group_by(year, prod_process) |>  
+  summarise(eggs = sum(n_eggs)) |>  
+  arrange(prod_process, year) 
 
+yoy <- annual_eggs |> 
+  group_by(prod_process) |> 
+  mutate(lag = lag(eggs, 1),
+         growth = (eggs - lag(eggs, 1)) / lag(eggs, 1)) |> 
+  filter(year >= 2017) |> 
+  ggplot(aes(x = year, y = growth, fill = prod_process)) +
+  geom_bar(stat = "identity", position = "dodge") + 
+  scale_fill_manual(values = c("#FB0000", "#EFAAA9", "#ECC301")) +
+  labs(x="", y = "Growth (% YoY)",
+       title = "Cracking the Egg Industry",
+       subtitle = "The proportion of cage-free eggs grew to 24% of total U.S. egg \nproduction after California implemented new confinement standards" ) + 
+  theme_classic() +
+  theme(
+    plot.title = element_text(face = "bold", size = 18),
+    plot.subtitle = element_text(size = 12),
+    plot.title.position = "plot",
+    #plot.caption.position = "plot",
+    plot.background = element_rect(fill = "#fffaf0", color = "#fffaf0"),
+    panel.background = element_rect(fill = "#fffaf0"),
+    axis.text.y = element_text(size = 8),
+    axis.text.x = element_text(size = 8),
+    legend.text = element_text(face = "bold", size = 10),
+    legend.title = element_blank(),
+    legend.background = element_rect(fill = "#fffaf0"),
+    legend.position = "bottom",
+    plot.margin = margin(.5, .5, 0, .5, "cm")
+  )
+
+# Combine graphs ---------------------------------------------------------------
+
+row <- plot_grid(vol, mix, nrow = 1, align = "v")
+plot_grid(yoy, row, ncol = 1, align = "h")
+
+ggsave("eggs.png", dpi=320)
